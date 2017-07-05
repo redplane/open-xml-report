c36484eab2c1eb28bd54c00b27e88aaf4c64592d
Linh Nguyen <redplane_dt@yahoo.com.vn>
2017-07-05 15:22:40 +0700

    - Finish implementing bkt-14 report.

--- OpenXmlProgram/Program.cs
+++ OpenXmlProgram/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -16,129 +17,100 @@ namespace OpenXmlProgram
 {
     internal class Program
     {
+
         private static void Main(string[] args)
         {
+            // Find application directory.
             var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            // Path to which file should be exported.
             var output = Path.Combine(path, $"{DateTime.Now.ToString("yy-MM-dd HH-mm-ss")}.xlsx");
-            var xmlFile = Path.Combine(path, "Data/bkt-14.xml");
-            var xml = File.ReadAllText(xmlFile);
-            
-            using (var spreadSheetDocument = SpreadsheetDocument.Create(output, SpreadsheetDocumentType.Workbook))
-            {
-                var workbookpart = spreadSheetDocument.AddWorkbookPart();
-                workbookpart.Workbook = new Workbook();
-                var worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
 
-                var stream = worksheetPart.GetStream();
-                var bytes = Encoding.UTF8.GetBytes(xml);
-                stream.Write(bytes, 0, bytes.Length);
-                workbookpart.Workbook.Save();
-                spreadSheetDocument.Close();
-            }
-            
-        }
-
-        protected static void AddPartXml(OpenXmlPart part, string xml)
-        {
-            using (Stream stream = part.GetStream())
-            {
-                byte[] buffer = (new UTF8Encoding()).GetBytes(xml);
-                stream.Write(buffer, 0, buffer.Length);
-            }
-        }
-
-        private static void Main1(string[] args)
-        {
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var output = Path.Combine(path, $"{DateTime.Now.ToString("yy-MM-dd HH-mm-ss")}.xlsx");
+            // Template file.
             path = Path.Combine(path, "Files/bkt-14.xlsx");
 
+            // Read all template data.
             var input = File.ReadAllBytes(path);
-            var memoryStream = new MemoryStream(input);
+            var memoryStream = new MemoryStream();
+            memoryStream.Write(input, 0, input.Length);
 
             // TODO: Remove
             var bktReport = FindReport();
 
+            if (bktReport.Station == null)
+            {
+                Console.WriteLine("Invalid station");
+                Console.ReadLine();
+                return;
+            }
+
+            // Find station in report.
+            var station = bktReport.Station;
+
+            // Data source sheet.
+            const string sheetDataSource = "Binding";
+
             using (var spreadSheetDocument = SpreadsheetDocument.Open(memoryStream, true))
             {
-                var sheets = FindSheets(spreadSheetDocument, x => x.Name.Value.Contains("Trang"));
-                foreach (var sheet in sheets)
+                // Find binding sheet.
+                var sheet = FindSheets(spreadSheetDocument, x => x.Name.Value.Equals(sheetDataSource)).FirstOrDefault();
+                spreadSheetDocument.WorkbookPart.Workbook.CalculationProperties.ForceFullCalculation = true;
+                spreadSheetDocument.WorkbookPart.Workbook.CalculationProperties.FullCalculationOnLoad = true;
+
+                // Find sheet attached information.
+                var part = FindWorksheetPart(spreadSheetDocument, sheet);
+                var rows = FindRows(part.Worksheet);
+
+                #region General information
+
+                var cellStationName = FindCell(rows, "E", 1);
+                if (cellStationName != null)
                 {
-                    var part = FindWorksheetPart(spreadSheetDocument, sheet);
-                    var rows = FindRows(part.Worksheet);
-
-                    #region General information
-
-                    var cellStationNames = FindCells(rows).Where(c => string.Compare
-                                                    (c.CellReference.Value, "E1", StringComparison.InvariantCultureIgnoreCase) == 0).ToList();
-
-                    var cellMonths = FindCells(rows).Where(c => string.Compare
-                                                                  (c.CellReference.Value, "K1", StringComparison.InvariantCultureIgnoreCase) == 0).ToList();
-
-                    var cellYears = FindCells(rows).Where(c => string.Compare
-                                                                    (c.CellReference.Value, "O1", StringComparison.InvariantCultureIgnoreCase) == 0).ToList();
-
-
-                    cellStationNames.ForEach(
-                        x =>
-                        {
-                            x.CellValue = new CellValue(bktReport.Station.Name);
-                            x.DataType = new EnumValue<CellValues>(CellValues.String);
-                        });
-
-                    cellMonths.ForEach(x =>
-                    {
-                        x.CellValue = new CellValue(bktReport.Time.Month.ToString());
-                        x.DataType = new EnumValue<CellValues>(CellValues.Number);
-                    });
-
-                    cellYears.ForEach(x =>
-                    {
-                        x.CellValue = new CellValue(bktReport.Time.Year.ToString());
-                        x.DataType = new EnumValue<CellValues>(CellValues.Number);
-                    });
-
-                    #endregion
-
-                    #region Report information fill
-
-                    var rowIndex = 6;
-                    while (true)
-                    {
-                        var cell = sheet.
-                            Descendants<Cell>().FirstOrDefault(c => c.CellReference == $"A{rowIndex}");
-
-                        if (cell == null)
-                            break;
-
-                        var iElementIndex = -1;
-                        if (!int.TryParse(cell.CellValue.InnerText, out iElementIndex))
-                            break;
-                        
-                        if (iElementIndex < 1 || iElementIndex > bktReport.Reports.Count)
-                            break;
-
-                        iElementIndex--;
-
-                        for (var alphabet = 'C'; alphabet <= 'Z'; alphabet++)
-                        {
-                            var vCell = sheet.
-                                Descendants<Cell>().FirstOrDefault(c => c.CellReference == $"{alphabet}{rowIndex}");
-
-                            if (vCell == null)
-                                continue;
-
-                            var iIndex = alphabet - 'C';
-                            var info = bktReport.Reports[iElementIndex][iIndex];
-                            vCell.CellValue = new CellValue(info.Rain.ToString("F"));
-                        }
-                    }
-
-                    #endregion
-
-                    part.Worksheet.Save();
+                    cellStationName.CellValue = new CellValue(station.Name);
+                    cellStationName.DataType = new EnumValue<CellValues>(CellValues.String);
                 }
 
+                var cellMonth = FindCell(rows, "K", 1);
+                if (cellMonth != null)
+                {
+                    cellMonth.CellValue = new CellValue(bktReport.Time.Month.ToString());
+                    cellMonth.DataType = new EnumValue<CellValues>(CellValues.Number);
+                }
+
+                var cellYear = FindCell(rows, "K", 1);
+                if (cellYear != null)
+                {
+                    cellYear.CellValue = new CellValue(bktReport.Time.Year.ToString());
+                    cellYear.DataType = new EnumValue<CellValues>(CellValues.Number);
+                }
+                
+                #endregion
+
+                #region Report information.
+
+                var time = bktReport.Time;
+                uint row = 6;
+
+                foreach (var dayReport in bktReport.Reports)
+                {
+                    for (var column = 'C'; column < 'Z'; column++)
+                    {
+                        var iColumnIndex = column - 'C';
+                        var cellRain = InsertCell($"{column}", row, part);
+                        var cellRainObs = InsertCell($"{column}", row + 1, part);
+
+                        cellRain.CellValue = new CellValue(dayReport[iColumnIndex].Rain.ToString());
+                        cellRain.DataType = new EnumValue<CellValues>(CellValues.Number);
+                        cellRainObs.CellValue = new CellValue(dayReport[iColumnIndex].RainObs.ToString());
+                        cellRainObs.DataType = new EnumValue<CellValues>(CellValues.Number);
+                    }
+
+                    row += 2;
+                }
+
+                #endregion
+
+                part.Worksheet.Save();
                 spreadSheetDocument.SaveAs(output);
             }
         }
@@ -223,6 +195,56 @@ namespace OpenXmlProgram
 
             var text = File.ReadAllText(path);
             return JsonConvert.DeserializeObject<BktFourteenReportViewModel>(text);
+        }
+
+        /// <summary>
+        /// Find cell at a specific position.
+        /// </summary>
+        /// <param name="rows"></param>
+        /// <param name="column"></param>
+        /// <param name="row"></param>
+        /// <returns></returns>
+        private static Cell FindCell(IEnumerable<Row> rows, string column, uint row)
+        {
+            return FindCells(rows).FirstOrDefault(c => string.Compare
+                                                                (c.CellReference.Value, $"{column}{row}", StringComparison.InvariantCultureIgnoreCase) == 0);
+        }
+
+        private static Cell InsertCell(string columnName, uint rowIndex, WorksheetPart worksheetPart)
+        {
+            var worksheet = worksheetPart.Worksheet;
+            var sheetData = worksheet.GetFirstChild<SheetData>();
+            string cellReference = columnName + rowIndex;
+            Row row;
+            if (sheetData.Elements<Row>().Count(r => r.RowIndex == rowIndex) != 0)
+            {
+                row = sheetData.Elements<Row>().First(r => r.RowIndex == rowIndex);
+            }
+            else
+            {
+                row = new Row() { RowIndex = rowIndex };
+                sheetData.Append(row);
+            }
+            if (row.Elements<Cell>().Any(c => c.CellReference.Value == columnName + rowIndex))
+            {
+                return row.Elements<Cell>().First(c => c.CellReference.Value == cellReference);
+            }
+            else
+            {
+                Cell refCell = null;
+                foreach (Cell cell in row.Elements<Cell>())
+                {
+                    if (String.Compare(cell.CellReference.Value, cellReference, StringComparison.OrdinalIgnoreCase) > 0)
+                    {
+                        refCell = cell;
+                        break;
+                    }
+                }
+                Cell newCell = new Cell() { CellReference = cellReference };
+                row.InsertBefore(newCell, refCell);
+                worksheet.Save();
+                return newCell;
+            }
         }
     }
 }
