3d7d5020e635a6b29e305a155e43e8c0d40f5e69
Linh Nguyen <redplane_dt@yahoo.com.vn>
2017-07-13 12:38:31 +0700

    - Update bkt-14 template.

--- OpenXmlProgram/Program.cs
+++ OpenXmlProgram/Program.cs
@@ -33,8 +33,20 @@ namespace OpenXmlProgram
             compiler.RegisterTag(new NumericCompareTagDefinition(), true);
             compiler.RegisterTag(new LookupTagDefinition(), true);
             compiler.RegisterTag(new LoopTagDefinition(), true);
+            
+            //ExportBktTen(path, compiler);
+            ExportBktFourteen(path, compiler);
+        }
 
-#if !USING_TXT
+        #region Methods
+
+        /// <summary>
+        /// Export bkt-10 report.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="formatCompiler"></param>
+        private static void ExportBktTen(string path, FormatCompiler formatCompiler)
+        {
             // Path to which file should be exported.
             var output = Path.Combine(path, $"{DateTime.Now.ToString("yy-MM-dd HH-mm-ss")}.xls");
             var input = Path.Combine(path, "Files/bkt-10.xml");
@@ -63,172 +75,55 @@ namespace OpenXmlProgram
                 detail.Reports = reports;
             }
 
-            var generator = compiler.Compile(szInput);
+            var generator = formatCompiler.Compile(szInput);
             var szXls = generator.Render(result);
 
             File.WriteAllText(output, szXls, Encoding.UTF8);
             Process.Start(output);
-#endif
+        }
 
-#if USING_TXT
+        /// <summary>
+        /// Export bkt-14 report.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="formatCompiler"></param>
+        private static void ExportBktFourteen(string path, FormatCompiler formatCompiler)
+        {
             // Path to which file should be exported.
-            var output = Path.Combine(path, $"bkt-10-{DateTime.Now.ToString("yy-MM-dd HH-mm-ss")}.txt");
-            var input = Path.Combine(path, "Files/bkt-10.txt");
+            var output = Path.Combine(path, $"{DateTime.Now.ToString("yy-MM-dd HH-mm-ss")}.xls");
+            var input = Path.Combine(path, "Files/bkt-14.xml");
 
             // Template file.
-            var szWindPath = Path.Combine(path, "Data/bkt-10.json");
-            var szText = File.ReadAllText(szWindPath);
+            var szRainPath = Path.Combine(path, "Data/bkt-14.json");
+            var szText = File.ReadAllText(szRainPath);
             var szInput = File.ReadAllText(input);
 
             // Deserialize text.
-            var result = JsonConvert.DeserializeObject<BktTenReportViewModel>(szText);
-            
-            var generator = compiler.Compile(szInput);
+            var result = JsonConvert.DeserializeObject<BktFourteenReportViewModel>(szText);
+            //foreach (var detail in result.Details)
+            //{
+            //    if (detail.Reports.Count > 22)
+            //        continue;
+
+            //    // Initiate new reports list.
+            //    var reports = new Wind[24];
+
+            //    for (var iReportIndex = 0; iReportIndex < detail.Reports.Count; iReportIndex++)
+            //    {
+            //        var report = detail.Reports[iReportIndex];
+            //        reports[report.DateTime.Hour] = report;
+            //    }
+
+            //    detail.Reports = reports;
+            //}
+
+            var generator = formatCompiler.Compile(szInput);
             var szXls = generator.Render(result);
 
             File.WriteAllText(output, szXls, Encoding.UTF8);
             Process.Start(output);
-#endif
         }
 
-        protected static void AddPartXml(OpenXmlPart part, string xml)
-        {
-            using (var stream = part.GetStream())
-            {
-                var buffer = (new UTF8Encoding()).GetBytes(xml);
-                stream.Write(buffer, 0, buffer.Length);
-            }
-        }
-
-        /// <summary>
-        ///     Find list of sheets by using specific conditions.
-        /// </summary>
-        /// <param name="spreadsheetDocument"></param>
-        /// <param name="condition"></param>
-        /// <returns></returns>
-        private static IList<Sheet> FindSheets(SpreadsheetDocument spreadsheetDocument, Func<Sheet, bool> condition)
-        {
-            // Find list of sheets.
-            return
-                spreadsheetDocument.WorkbookPart.Workbook.Sheets.Elements<Sheet>().Where(condition).ToList();
-        }
-
-        /// <summary>
-        ///     Find worksheet part by using sheet information.
-        /// </summary>
-        /// <param name="spreadsheetDocument"></param>
-        /// <param name="sheet"></param>
-        /// <returns></returns>
-        private static WorksheetPart FindWorksheetPart(SpreadsheetDocument spreadsheetDocument, Sheet sheet)
-        {
-            var worksheetPart = (WorksheetPart)
-                spreadsheetDocument.WorkbookPart.GetPartById(sheet.Id.Value);
-            return worksheetPart;
-        }
-
-        /// <summary>
-        ///     Find rows in worksheet by using specific conditions.
-        /// </summary>
-        /// <param name="worksheet"></param>
-        /// <returns></returns>
-        private static IEnumerable<Row> FindRows(Worksheet worksheet)
-        {
-            return worksheet.Elements<SheetData>().SelectMany(x => x.Elements<Row>());
-        }
-
-        /// <summary>
-        ///     Find rows in worksheet by using specific conditions.
-        /// </summary>
-        /// <param name="worksheets"></param>
-        /// <param name="condition"></param>
-        /// <returns></returns>
-        private static IEnumerable<Row> FindRows(IEnumerable<Worksheet> worksheets, Func<Row, bool> condition)
-        {
-            return worksheets.SelectMany(worksheet => worksheet.Elements<SheetData>()
-                .SelectMany(x => x.Elements<Row>().Where(condition)));
-        }
-
-        /// <summary>
-        ///     Find cells from collection of rows.
-        /// </summary>
-        /// <param name="rows"></param>
-        /// <returns></returns>
-        private static IEnumerable<Cell> FindCells(IEnumerable<Row> rows)
-        {
-            return rows.SelectMany(x => x.Elements<Cell>());
-        }
-
-
-        /// <summary>
-        ///     Find cells by using specific conditions from collection of rows.
-        /// </summary>
-        /// <param name="rows"></param>
-        /// <param name="condition"></param>
-        /// <returns></returns>
-        private static IEnumerable<Cell> FindCells(IEnumerable<Row> rows, Func<Cell, bool> condition)
-        {
-            return rows.SelectMany(x => x.Elements<Cell>().Where(condition));
-        }
-
-        /// <summary>
-        ///     Find report information.
-        /// </summary>
-        /// <returns></returns>
-        private static BktFourteenReportViewModel FindReport()
-        {
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            path = Path.Combine(path, "Data/bkt-14.json");
-
-            var text = File.ReadAllText(path);
-            //return JsonConvert.DeserializeObject<BktFourteenReportViewModel>(text);
-            return null;
-        }
-
-
-        /// <summary>
-        ///     Find cell at a specific position.
-        /// </summary>
-        /// <param name="rows"></param>
-        /// <param name="column"></param>
-        /// <param name="row"></param>
-        /// <returns></returns>
-        private static Cell FindCell(IEnumerable<Row> rows, string column, uint row)
-        {
-            return FindCells(rows).FirstOrDefault(c => string.Compare
-                                                       (c.CellReference.Value, $"{column}{row}",
-                                                           StringComparison.InvariantCultureIgnoreCase) == 0);
-        }
-
-        private static Cell InsertCell(string columnName, uint rowIndex, WorksheetPart worksheetPart)
-        {
-            var worksheet = worksheetPart.Worksheet;
-            var sheetData = worksheet.GetFirstChild<SheetData>();
-            var cellReference = columnName + rowIndex;
-            Row row;
-            if (sheetData.Elements<Row>().Count(r => r.RowIndex == rowIndex) != 0)
-            {
-                row = sheetData.Elements<Row>().First(r => r.RowIndex == rowIndex);
-            }
-            else
-            {
-                row = new Row { RowIndex = rowIndex };
-                sheetData.Append(row);
-            }
-            if (row.Elements<Cell>().Any(c => c.CellReference.Value == columnName + rowIndex))
-            {
-                return row.Elements<Cell>().First(c => c.CellReference.Value == cellReference);
-            }
-            Cell refCell = null;
-            foreach (var cell in row.Elements<Cell>())
-                if (string.Compare(cell.CellReference.Value, cellReference, StringComparison.OrdinalIgnoreCase) > 0)
-                {
-                    refCell = cell;
-                    break;
-                }
-            var newCell = new Cell { CellReference = cellReference };
-            row.InsertBefore(newCell, refCell);
-            worksheet.Save();
-            return newCell;
-        }
+        #endregion
     }
 }
